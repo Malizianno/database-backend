d8b203dd4dfe1cffaafd0f5a8bbded3b85a62d45
cristian.sterie <cristian.sterie@technohub.ro>
2026-06-21 13:26:20 +0300

    add maven project with spring boot 4.1;

---
+++ src/main/java/ro/cristiansterie/database_backend/DatabaseBackendApplication.java
@@ -0,0 +1,13 @@
+package ro.cristiansterie.database_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseBackendApplication.class, args);
+	}
+
+}
